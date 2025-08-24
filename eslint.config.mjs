3eef0077da718d1d81857084a10837a7f3595f50
syrnjs <ksy21019@gmail.com>
2025-08-25 04:54:18 +0900

    chore: 초기 세팅

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import prettier from 'eslint-plugin-prettier'
+import configPrettier from 'eslint-config-prettier'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  },
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  configPrettier,
+]
+
+export default eslintConfig
